a42d86df4e1486e04acb0aa636b78b3bf646e224
Talukdar,Partha <partha.talukdar@vantagecircle.com>
2020-02-18 22:58:18 +0530

    WIP

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -44,4 +44,13 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
 
   }
+
+  @Test
+  public void greetShouldIncludeGreetingMessageThree() {
+    String someone = "World, Thanks you making me Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+
+  }
+
 }
